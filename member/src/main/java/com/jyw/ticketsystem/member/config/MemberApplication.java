5a45bd471ece8ce25420cefad168589857c5856b
jyw <jywang_gloria616@163.com>
2023-05-12 00:21:19 +0800

    23-05-12 修改启动日志，新增/member，修改logback日志

--- member/src/main/java/com/jyw/ticketsystem/member/config/MemberApplication.java
+++ member/src/main/java/com/jyw/ticketsystem/member/config/MemberApplication.java
@@ -1,16 +1,23 @@
 package com.jyw.ticketsystem.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 //默认扫描当前包和子孙包的所有类
 @ComponentScan("com.jyw")
 public class MemberApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+    public static void main(String[] args){
+        SpringApplication app=new SpringApplication(MemberApplication.class);
+        Environment env= app.run(args).getEnvironment();
+        LOG.info("启动成功啦~");
+        LOG.info("项目地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
+
 
 }
