6d794d6b8e9c801b5455f41670a51d83c747696b
jyw <jywang_gloria616@163.com>
2023-05-29 20:46:08 +0800

    集成Ant Design Vue

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import store from './store'
+import * as Icons from '@ant-design/icons-vue';
 
 /*'#app'是跟index.html里div标签绑定的，内容会渲染到div中*/
-createApp(App).use(store).use(router).mount('#app')
+const app=createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+const icons= Icons;
+for(const i in icons){
+    app.component(i,icons[i]);
+}
