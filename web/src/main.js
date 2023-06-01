38e0b40f856891626783209790f8104e5a8e6ae2
jyw <jywang_gloria616@163.com>
2023-06-01 21:47:39 +0800

    CLI多环境配置

--- web/src/main.js
+++ web/src/main.js
@@ -30,3 +30,6 @@ axios.interceptors.response.use(function (response) {
     console.log('返回错误：', error);
     return Promise.reject(error);
 });
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -52,7 +52,7 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/send-code", {
+      axios.post("/member/member/send-code", {
         mobile: loginForm.mobile
       }).then(response => {
         let data = response.data;
@@ -66,7 +66,8 @@ export default defineComponent({
     };
 
     const login = () => {
-      axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
+      //应该实现多环境配置，不能写死
+      axios.post("/member/member/login", loginForm).then((response) => {
         let data = response.data;
         if (data.success) {
           notification.success({ description: '登录成功！' });
