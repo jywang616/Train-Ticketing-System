84289c442b1582ec3c0077ef2a291709b1480c87
jyw <jywang_gloria616@163.com>
2023-06-04 12:18:56 +0800

    增加字段非空校验

--- member/src/main/java/com/jyw/ticketsystem/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/jyw/ticketsystem/member/req/PassengerSaveReq.java
@@ -1,16 +1,23 @@
 package com.jyw.ticketsystem.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 
 public class PassengerSaveReq {
     private Long id;
 
+    @NotNull(message = "会员id不能为空")
     private Long memberId;
 
+    @NotBlank(message = "乘车人姓名不能为空")
     private String name;
 
+    @NotBlank(message = "身份证不能为空")
     private String idCard;
 
+    @NotBlank(message = "乘车人类型不能为空")
     private String type;
 
     private Date createTime;
