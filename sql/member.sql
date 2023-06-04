672d044c2f47d8634988ea32d608ab9b2c562192
jyw <jywang_gloria616@163.com>
2023-06-04 11:49:36 +0800

    增加乘车人表，生成持久层代码

--- sql/member.sql
+++ sql/member.sql
@@ -6,3 +6,15 @@ create table `member` (
                           unique key `mobile_unique` (`mobile`)
 ) engine=innodb default charset=utf8mb4 comment='会员';
 
+drop table if exists `passenger`;
+create table `passenger` (
+                             `id` bigint not null comment 'id',
+                             `member_id` bigint not null comment 'member表id',
+                             `name` varchar(20) not null comment '姓名',
+                             `id_card` varchar(18) not null comment '身份证',
+                             `type` char(1) not null comment '旅客类型|枚举[PassengerTypeEnum]',
+                             `create_time` datetime(3) comment '新增时间',
+                             `update_time` datetime(3) comment '修改时间',
+                             primary key (`id`),
+                             index `member_id_index` (`member_id`)
+) engine=innodb default charset=utf8mb4 comment='乘车人';
