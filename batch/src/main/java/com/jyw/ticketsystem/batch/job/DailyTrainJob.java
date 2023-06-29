7e704b5ea469edfd5eeed84cb00553d5ce9f9b72
jyw <jywang_gloria616@163.com>
2023-06-29 17:56:35 +0800

    增加每日车次生成定时任务

---
+++ batch/src/main/java/com/jyw/ticketsystem/batch/job/DailyTrainJob.java
@@ -0,0 +1,24 @@
+package com.jyw.ticketsystem.batch.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("开始生成每日车次数据");
+        LOG.info("生成每日车次数据结束");
+    }
+}
